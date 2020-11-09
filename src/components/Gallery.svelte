a901833749b0ed09884623543a70e14624285bb7
koehr <n@koehr.in>
2020-11-09 13:29:18 +0100

    adds indexpage scaffolds and gallery

---
+++ src/components/Gallery.svelte
@@ -0,0 +1,162 @@
+<script>
+  export let items = []
+  export let currentIndex = 0
+
+  let nextCurrent = currentIndex
+  let transitionToNextImage = false
+  let transitionToPrevImage = false
+
+  const gesturing = {
+    active: false,    // activates move event
+    cancelled: false, // enables transitioning back to original state
+    threshholdX: 350, // pixels to move to trigger image switch
+    threshholdT: 200, // max milliseconds to trigger immediate switch
+    startX: 0,        // we're only interested in the x-axis
+    startTime: 0,     // to detect fast swipes
+    deltaX: 0,        // used to calculate shift on touchMove
+  }
+
+  $: lastItemIndex = items.length - 1
+  $: prevItemIndex = currentIndex > 0 ? currentIndex - 1 : lastItemIndex
+  $: nextItemIndex = currentIndex < lastItemIndex ? currentIndex + 1 : 0
+
+  $: currentItems = [
+    items[prevItemIndex],
+    items[currentIndex],
+    items[nextItemIndex]
+  ]
+
+  $: cssVars = `
+    --shift-left: ${gesturing.deltaX < 0 ? gesturing.deltaX : 0}px;
+    --shift-right: ${gesturing.deltaX > 0 ? gesturing.deltaX : 0}px;
+    --transition-time: ${gesturing.cancelled ? 300 : 0}ms;
+  `
+
+  function prevItem () {
+    transitionToPrevImage = true
+    nextCurrent = prevItemIndex
+  }
+
+  function nextItem () {
+    transitionToNextImage = true
+    nextCurrent = nextItemIndex
+  }
+
+  function transitionEnd () {
+    currentIndex = nextCurrent
+    transitionToNextImage = false
+    transitionToPrevImage = false
+    gesturing.cancelled = false
+  }
+
+  function handleTouchStart (event) {
+    event.stopImmediatePropagation()
+    event.stopPropagation()
+    gesturing.active = true
+    gesturing.cancelled = false
+    gesturing.startX = event.x
+    gesturing.startTime = event.timeStamp
+  }
+  function handleTouchEnd (event) {
+    event.stopImmediatePropagation()
+    event.stopPropagation()
+    gesturing.active = false
+    gesturing.deltaX = 0
+
+    const deltaX = event.x - gesturing.startX
+    const deltaT = event.timeStamp - gesturing.startTime
+    const { threshholdT, threshholdX } = gesturing
+    const isFast = Math.abs(deltaX) > 10 && deltaT < threshholdT
+    const isFar = Math.abs(deltaX) > threshholdX
+    gesturing.cancelled = !isFast && !isFar
+
+    if (!gesturing.cancelled) {
+      if (deltaX < 0) nextItem()
+      else prevItem()
+    }
+  }
+  function handleTouchMove (event) {
+    if (!gesturing) return
+    event.stopImmediatePropagation()
+    event.stopPropagation()
+    gesturing.deltaX = event.x - gesturing.startX
+  }
+</script>
+
+<style>
+.gallery-strip {
+  display: flex;
+  flex-flow: row nowrap;
+  width: 100%;
+}
+
+.image-container {
+  flex: 0 0 auto;
+  justify-content: center;
+  width: 80%;
+  margin: 0;
+  display: flex;
+  flex-flow: row nowrap;
+  overflow: hidden;
+}
+.image-container > img {
+  flex: 0 0 auto;
+  width: 100%;
+  pointer-events: none;
+  transition: transform var(--transition-time) ease-in;
+}
+.image-container > img.prev {
+  transform: translate(var(--shift-right));
+}
+.image-container > img.next {
+  transform: translate(var(--shift-left));
+}
+
+.image-container.prev > img.prev ,
+.image-container.next > img.next {
+  transition: transform .4s ease-in-out;
+}
+.image-container.prev > img.prev {
+  transform: translate(100%);
+}
+.image-container.next > img.next {
+  transform: translate(-100%);
+}
+
+.gallery-action {
+  flex: 0 0 auto;
+  width: 10%;
+  border: none;
+  background-color: transparent;
+  font-size: 2em;
+  color: #CCD;
+  cursor: pointer;
+  transition: background-color .3s ease-in;
+}
+.gallery-action:hover {
+  color: #FFF;
+  background-color: #333;
+}
+</style>
+
+<div class="gallery-strip" style={cssVars}>
+  <button class="gallery-action prev" on:click={prevItem}> ❰ </button>
+  <div class="image-container"
+    class:next={transitionToNextImage}
+    class:prev={transitionToPrevImage}
+    on:pointerdown={handleTouchStart}
+    on:pointerup={handleTouchEnd}
+    on:pointermove={handleTouchMove}
+    on:transitionend={transitionEnd}
+  >
+    {#each currentItems as {src, alt}, i (src)}
+    <img
+      class:prev={i === 0}
+      class:next={i === 2}
+      {src}
+      {alt}
+    />
+    {/each}
+  </div>
+  <button class="gallery-action next" on:click={nextItem}> ❱ </button>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,9 +2,37 @@
   import LogoHeader from '../components/LogoHeader.svelte'
   import SocialIcons from '../components/SocialIcons.svelte'
   import ScrollHint from '../components/ScrollHint.svelte'
+  import Gallery from '../components/Gallery.svelte'
+
+  let galleryItems = [
+    { src: '/gallery/01.jpg', alt: 'Lorem Ipsum sit...' },
+    { src: '/gallery/02.jpg', alt: 'Lorem Ipsum dolor...' },
+    { src: '/gallery/03.jpg', alt: 'Lorem Ipsum amet...' },
+    { src: '/gallery/04.jpg', alt: 'Lorem Ipsum consetetur...' },
+  ]
 </script>
 
 <style>
+#landing > main > section {
+  display: block;
+  height: 100vh;
+  width: 100vw;
+  margin: 5vh auto;
+  max-width: 960px;
+}
+
+section[name="introduction"] {
+  column-count: 2;
+  column-gap: 4em;
+  text-align: justify;
+}
+section[name="introduction"] > header {
+  column-span: all;
+  margin-bottom: 1.5em;
+}
+section[name="introduction"] > p:first-of-type {
+  margin-top: 0;
+}
 </style>
 
 <svelte:head>
@@ -12,12 +40,46 @@
 </svelte:head>
 
 <div id="landing">
+
   <LogoHeader>
     <span slot="headline">Lasertag Verein Berlin</span>
     <SocialIcons />
     <ScrollHint />
   </LogoHeader>
+
   <main>
-    <h1>Willkommen beim Lasertag Verein</h1>
+
+    <section name="introduction">
+      <header>
+        <h1>Willkommen beim Lasertag Verein</h1>
+      </header>
+      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+      <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+      <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+      <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+      <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+    </section>
+
+    <section name="gallery">
+      <header>
+        <h1>Bilder?!</h1>
+      </header>
+      <Gallery items={galleryItems} />
+    </section>
+
+    <section name="locked">
+      <!-- competitions / leaderboards -->
+    </section>
+
+    <section name="faq">
+      <header>
+        <h1>Fragen und Antworten</h1>
+      </header>
+      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet</p>.
+      <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat</p>.
+      <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p> 
+    </section>
+
   </main>
+
 </div>
